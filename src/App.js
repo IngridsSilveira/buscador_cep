7cf58299e6aef6463d4dd0f1aee08a66b118017e
IngridsSilveira <ingridsouzaok@gmail.com>
2023-05-04 12:03:35 -0300

    finalizado

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import "./App.css";
+
 export default function App() {
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState({});
+
+  const handleSearch = async () => {
+    if (input === "") {
+      alert("Preencha com seu cep");
+      return;
+    }
+    try {
+      await fetch(`https://viacep.com.br/ws/${input}/json`)
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+          setCep(res);
+          setInput("");
+        });
+    } catch {
+      alert("Ops, erro a buscar");
+      setInput("");
+    }
+  };
+
   return (
     <div className="container">
-      <h1 className='title'>Buscador CEP</h1>
+      <h1 className="title">Buscador de CEP</h1>
 
-      <div className='continerInput'>
+      <div className="containerInput">
         <input
-        type='text'
-        placeholder='Digite seu cep...'
+          type="text"
+          placeholder="Digite seu CEP..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
         />
 
-        <button className='buttonSearch'></button>
-
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#FFF" />
+        </button>
       </div>
 
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>{cep.bairro}</span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
+      <footer>
+        <p>Feito por <a href="https://ingriddev.netlify.app/">Ingrid de Souza</a></p>
+      </footer>
     </div>
   );
 }
